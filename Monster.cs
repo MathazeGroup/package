19d8f1050aff088c146fbab18e49e92596aa860c
サイラク <episodes27@gmail.com>
2016-04-09 23:40:48 +1000

    Monster类(未完成)

---
+++ Monster.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class MonsterInfo
+{
+    [Header("怪物名字")]
+    public string Name;
+    [Header("怪物等级")]
+    public int Level;
+
+
+}
+public class Monster : MonoBehaviour 
+{
+    public MonsterInfo[] MonsterData;
+   
+ 
+}
