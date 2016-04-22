4676076394e4044f776a252dc31b0ed47f98b869
サイラク <episodes27@gmail.com>
2016-04-22 15:07:05 +1000

    2.3

---
+++ GlobalAudio.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalAudio : MonoBehaviour {
+
+    public bool IsMute;
+    // Use this for initialization
+
+    void Start()
+    {
+        int toggle = PlayerPrefs.GetInt("SFX");
+        if (toggle == 1) IsMute = true;
+        else IsMute = false;
+        TunVol();
+    }
+
+    public void ToogleVol()
+    {
+        if (IsMute)
+        {
+            IsMute = false;
+            PlayerPrefs.SetInt("SFX", 0);
+            TunVol();
+        }
+        else
+        {
+            IsMute = true;
+            PlayerPrefs.SetInt("SFX", 1);
+            TunVol();
+        }
+    }
+	
+    public void TunVol()
+    {
+        if(IsMute) Camera.main.gameObject.GetComponent<AudioSource>().mute = true;
+        else Camera.main.gameObject.GetComponent<AudioSource>().mute = false; ;
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
